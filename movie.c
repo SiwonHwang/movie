4e1889691ca36dc8ef9ac29021ddee7e9bf5369d
SiwonHwang <kimshluv@naver.com>
2018-12-10 23:47:48 +0900

    12/10(2)

--- movie.c
+++ movie.c
@@ -4,11 +4,11 @@
 #include "movie.h"
 
 typedef struct movInfo{
-	char name[200];
-	float score;
-	int runTime;
-	char madeIn[10];
-} movInfo_t;
+	char name[200]; //영화 이름 
+	float score; //영화 평점 
+	int runTime; //영화 런타임 
+	char madeIn[10]; //영화 제작 국가 
+} movInfo_t; //구조체 movInfo를 movInfo_t로 정의 
 
 
 void* mv_genMvInfo(char* name, float score, int runTime, char* country)
@@ -16,12 +16,12 @@ void* mv_genMvInfo(char* name, float score, int runTime, char* country)
 	movInfo_t* mvPtr;
 	
 	//allocate memory and set the member variables 
-	//메모리 할당 -> malloc 함수 사용 
+	//메모리 동적 할당 -> malloc 함수 사용 
 	mvPtr = (movInfo_t*)malloc( sizeof(movInfo_t) ); 
 	
 	if (mvPtr == NULL)
 	{
-		printf("메모리 할당 오류\n");
+		printf("ERROR! 메모리 할당 오류\n");
 	}
 	
 	//변수값 할당 -> 구조체는 strcpy 이용 
@@ -42,8 +42,8 @@ void mv_print(void* obj)
 		printf("[ERROR] failed to print the movie Info! (object is NULL)\n");
 	}
 	
-	printf("Name : %s (%s)\n", mvPtr->name, mvPtr->madeIn);
-	printf("running time : %i, score : %f\n", mvPtr->runTime, mvPtr->score);
+	printf("Name : %s (%s)\n", mvPtr->name, mvPtr->madeIn); //영화 이름과 나라 출력  
+	printf("running time : %i, score : %f\n", mvPtr->runTime, mvPtr->score); //영화 런타임과 평점 출력 
 	
 	return;
 }
@@ -52,25 +52,25 @@ void mv_print(void* obj)
 //return the score value from the input instance of movInfo_t structure
 float mv_getScore(void* obj)
 {
-	return (movInfo_t->score);
+	return ( ((movInfo_t*)obj)->score);
 }
 
 //return the runtime value from the input instance of movInfo_t structure
 int mv_getRunTime(void* obj)
 {
-	return (movInfo_t->runTime);
+	return ( ((movInfo_t*)obj)->runTime);
 }
 
 //return the name string pointer from the input instance of movInfo_t structure
 char* mv_getName(void* obj)
 {
-	return (movInfo_t->name);
+	return ( ((movInfo_t*)obj)->name);
 }
 
 //return the country string pointer from the input instance of movInfo_t structure
 char* mv_getCountry(void* obj)
 {
-	return (movInfo_t->madeIn);
+	return ( ((movInfo_t*)obj)->madeIn);
 }
 
 
